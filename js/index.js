7dff0b0903cabbdb5291a826ec4945fa63208329
Andrey Mazépas <andreymazepas@gmail.com>
2022-07-21 19:55:12 -0300

    Refactoring

--- js/index.js
+++ js/index.js
@@ -1,11 +1,4 @@
-var renderer,
-  scene,
-  camera,
-  composer,
-  sphere,
-  container,
-  backgroundbox,
-  particle;
+let renderer, scene, camera, composer, sphere, container;
 
 window.onload = function () {
   init();
@@ -28,21 +21,19 @@ function init() {
   camera.position.z = 40;
   scene.add(camera);
 
-  var geom = new THREE.OctahedronGeometry(7, 1);
-  var geom2 = new THREE.BoxGeometry(15, 15, 15, 1, 1, 1);
+  let geom = new THREE.OctahedronGeometry(7, 1);
 
-  var mat = new THREE.MeshPhongMaterial({
+  const mat = new THREE.MeshPhongMaterial({
     color: 0xffffff,
   });
-  var material = new THREE.MeshNormalMaterial();
 
-  var mat2 = new THREE.MeshPhongMaterial({
+  const mat2 = new THREE.MeshPhongMaterial({
     color: 0xffffff,
     wireframe: true,
     side: THREE.DoubleSide,
   });
 
-  var sphere_geometry = new THREE.SphereGeometry(1, 128, 128);
+  let sphere_geometry = new THREE.SphereGeometry(1, 128, 128);
   sphere = new THREE.Mesh(sphere_geometry, mat);
   sphere.scale.x = sphere.scale.y = sphere.scale.z = 5;
   scene.add(sphere);
@@ -50,15 +41,7 @@ function init() {
   container = new THREE.Mesh(geom, mat2);
   scene.add(container);
 
-  var boxMesh = new THREE.Mesh(geom2, material);
-  boxMesh.doubleSided = true;
-  boxMesh.scale.x = boxMesh.scale.y = boxMesh.scale.z = 15;
-  //backgroundbox.add(boxMesh);
-
-  var ambientLight = new THREE.AmbientLight(0x999999);
-  //  scene.add(ambientLight);
-
-  var lights = [];
+  let lights = [];
   lights[0] = new THREE.DirectionalLight(0xffffff, 0.5);
   lights[0].position.set(1, 0, 0);
   lights[1] = new THREE.DirectionalLight(0xad5389, 0.5);
@@ -79,11 +62,11 @@ function onWindowResize() {
 }
 
 function update() {
-  var time = performance.now() * 0.001;
+  const time = performance.now() * 0.001;
 
-  var k = 2;
-  for (var i = 0; i < sphere.geometry.vertices.length; i++) {
-    var p = sphere.geometry.vertices[i];
+  const k = 2;
+  for (let i = 0; i < sphere.geometry.vertices.length; i++) {
+    let p = sphere.geometry.vertices[i];
     p.normalize().multiplyScalar(
       1 + 0.3 * noise.perlin3(p.x * k + time, p.y * k, p.z * k)
     );
